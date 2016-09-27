e0a4a889cb6264749a3ec94bc4c066038fe67126
gplessm <guido.plessmann@rl-institut.de>
2016-09-27 23:43:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='europepstrans',
+    version='0.0.1-pre',
+    packages=[''],
+    url='',
+    license='GPL v3',
+    author='Guido Pleßmann',
+    author_email='guido.plessmann@rl-institut.de',
+    description='European power sector long-term investment model',
+    requires=['pandas >= 0.18.1']
+)
