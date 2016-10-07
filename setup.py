c6a4905269c3bd046fe84dfcdc31bbe5f8689d03
gplessm <guido.plessmann@rl-institut.de>
2016-10-07 08:59:50 +0200

    Adapt setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     author='Guido Pleßmann',
     author_email='guido.plessmann@rl-institut.de',
     description='European power sector long-term investment model',
-    install_requires=['pandas >= 0.18.1',
-              'matplotlib']
+    install_requires=[
+        'pandas >= 0.18.1',
+        'matplotlib',
+        'oemof >= 0.0.8.dev0']
 )
